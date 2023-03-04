31469c42ab6c3dcdb3b949599d4a446d920e598b
diegomuner <diegomuner@hotmail.com>
2023-03-04 18:00:17 -0300

    updated email to include @ so test can successfully sign up a new user

--- src/test/java/com/casino/tests/casinoTests.java
+++ src/test/java/com/casino/tests/casinoTests.java
@@ -16,7 +16,10 @@ public class casinoTests extends BaseTest {
 Faker faker = new Faker();
 
 // Because faker.fullname() returns a String with spaces we remove spaces to use that as an email
-String email = ((faker.name().fullName()).replaceAll("\\s+", "").replaceAll("[^a-zA-Z0-9]", "").toLowerCase())+"@something";
+
+// This is declared here so we can use it across multiple tests
+// Can also be generated in the webpage class and using a setter and getter return it and store it a variable in this class as well 
+String email = ((faker.name().fullName()).replaceAll("\\s+", "").replaceAll("[^a-zA-Z0-9]", "").toLowerCase())+"@gmail.com";
 String password = "SuperSecret123!!";
 	
 	
@@ -41,14 +44,15 @@ String password = "SuperSecret123!!";
 		
 		//Or we can just do it here in the test
 	    // send keys to send the Strings declared at the top
+		//We could also input wrong information like an email with no @ to validate error messages and that FE validations are in place.
 		home.emailField.sendKeys(email);
 		home.passwordField.sendKeys(password);
 		home.passwordField2.sendKeys(password);
+		//We could potentially also chech the endpoint that the FE hits and build a request with bad data to the endpoint and check that there are also BE validations in place.
 		home.checkboxOlder.click();
 		Thread.sleep(3000);
 		home.checkboxNoBonus.click();
 		home.createAccount.click();
-		
 		Thread.sleep(10000);
 		
 		
